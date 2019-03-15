9adb67be819083fff936373cb0c4fadfa572d278
Kasza <chris.kasza@gmail.com>
2019-03-14 22:29:50 -0300

    Populate chart via props

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,28 +1,18 @@
 <script>
-// import VueCharts from 'vue-chartjs'
 // import { Bar, Line } from 'vue-chartjs'
 import { Line } from 'vue-chartjs'
 
 export default {
   extends: Line,
+  props: {
+    chartdata: { type: Object, default: null },
+    options: {
+      type: Object,
+      default: () => { return {responsive: true, maintainAspectRatio: false }}
+    }
+  },
   mounted() {
-    // Overwriting base render method with actual data.
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: [10, 20, 12, 3, 10, 4, 3, 8, 4, 2, 12, 11]
-        },
-        {
-          label: 'Other stuff',
-          backgroundColor: '#f87979',
-          data: [10, 23, 32, 3, 10, 4, 3, 8, 4, 2, 32, 11]
-        }
-      ]
-    },
-    { responsive: true, maintainAspectRatio: false })
+    this.renderChart(this.chartdata, this.options)
   }
 }
 </script>
